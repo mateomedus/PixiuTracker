4fd74a48885e2af1c9ab3b64924533f675dcf2db
agustinstr <agus.stragua@gmail.com>
2021-09-04 01:53:28 -0300

    Initial migration

---
+++ DatabaseContext/Migrations/20210904045023_Initial.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace DatabaseContext.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BinanceUser",
+                columns: table => new
+                {
+                    Username = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: false),
+                    FirstName = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false),
+                    LastName = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false),
+                    Password = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    Email = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    ApiKey = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    ApiSecret = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    PorfolioID = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BinanceUser", x => x.Username);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Coin",
+                columns: table => new
+                {
+                    Name = table.Column<string>(type: "character varying(15)", maxLength: 15, nullable: false),
+                    Price = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Coin", x => x.Name);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Portfolio",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CoinID = table.Column<int>(type: "integer", nullable: false),
+                    Amount = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Portfolio", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BinanceUser_Email",
+                table: "BinanceUser",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BinanceUser_Username_Password",
+                table: "BinanceUser",
+                columns: new[] { "Username", "Password" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Coin_Id",
+                table: "Coin",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BinanceUser");
+
+            migrationBuilder.DropTable(
+                name: "Coin");
+
+            migrationBuilder.DropTable(
+                name: "Portfolio");
+        }
+    }
+}
